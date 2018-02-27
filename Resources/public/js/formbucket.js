2755651c89d4ffaf0c4586fb4c9fbecfe6e89842
Adesh <adesh@akhann.com>
2018-02-27 00:22:53 -0800

    Add valiaton callback in js

--- Resources/public/js/formbucket.js
+++ Resources/public/js/formbucket.js
@@ -1,46 +1,53 @@
-$(function() {
-  $('form[data-formbucket]').each(function(){
-    var $this = $(this);
-    var options = $this.data('options');
+(function ( $ ) {
+  $.fn.akFormBucket = function(options) {
+    return this.each(function() {
 
-    $($this).submit(function(event) {
-    event.preventDefault();
+      var settings = $.extend({
+        //deafult settings
+        "successClass": ".success", 
+        "errorClass": ".error" ,
+        "validate": null
+      }, options);
 
-    var subscribeForm = $(this);
-    var subscribeButton = $('input[type=submit]', subscribeForm);
-    subscribeButton.prop('disabled', false);
-    var classSuccess = '.success';
-    var classError = '.error';
+      $(this).submit(function(event) {
+        event.preventDefault();
 
+        if (settings.validate && !settings.validate(this)) {
+            return;
+        }
 
-    $.ajax({
-      url: subscribeForm.prop('action'),
-      type: 'POST',
-      crossDomain: true,
-      headers : {
-        'accept' : 'application/javascript',
-      },
-      data: $($this).serialize(),
-      beforeSend: function() {
-      subscribeButton.prop('disabled', 'disabled');
-    }
-    })
+        var subscribeForm = $(this);
+        var subscribeButton = $('input[type=submit]', subscribeForm);
+        subscribeButton.prop('disabled', false);
+        var classSuccess = '.success';
+        var classError = '.error';
+          
+        $.ajax({
+          url: subscribeForm.prop('action'),
+          type: 'POST',
+          crossDomain: true,
+          headers : {
+            'accept' : 'application/javascript',
+          },
+          data: $($(this)).serialize(),
+          beforeSend: function() {
+          subscribeButton.prop('disabled', 'disabled');
+        }
+        })
 
-    
-    .done(function(response) {
-      //alert('Thanks for subscribing!');
-      subscribeForm.hide();
-      $(options.successClass).show();
-    })
+        .done(function(response) {
+          //alert('Thanks for subscribing!');
+          subscribeForm.hide();
+          $(settings.successClass).show();
+        })
 
-    .fail(function(response) {
-      //alert('Dang, something went wrong!');
-      $(options.errorClass).show();
-      subscribeButton.prop('disabled', false);
-    })
+        .fail(function(response) {
+          //alert('Dang, something went wrong!');
+          $(settings.errorClass).show();
+          subscribeButton.prop('disabled', false);
+        })
 
-  });
-  });
-});
-
-   
+       });
+    });
+  };
+}( jQuery ));
